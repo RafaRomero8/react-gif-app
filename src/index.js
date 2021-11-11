5c7444ec0ae84f7df08bcde34edaee75ac64d85e
RafaRomero8 <RafaRomerox@gmail.com>
2021-11-10 18:02:11 -0600

    Renombramos carpeta build por docs

--- src/index.js
+++ src/index.js
@@ -13,8 +13,11 @@ ReactDOM.render(
 //checar urls
 //https://hackernoon.com/structuring-projects-and-naming-components-in-react-1261b6e18d76
 
-//se ocupa el directorio build para desplegarlo en un sitio web
-//se utiliza el comando npm start build para que se cree
 
 //los custom hook es una forma de extraer logica de un compenente que quiero utilizar
 //y extraerla de tal manera que se a sencillo utilizarla nuevamente
+
+//se ocupa el directorio build para desplegarlo en un sitio web
+//se utiliza el comando npm start build para que se cree
+//al abrir el html nos dara error porque no esta en un servidor como tal
+//antes de subirlo al github cambiamos el nombre de la carpeta build a docs para github para poder desplegar el repo o aplicaciones http
